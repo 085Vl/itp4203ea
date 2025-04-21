5719fd21433d7f3e4e6d9156b0fd66e15e86582a
085Vl <lauvenus0805@gmail.com>
2025-04-22 04:47:39 +0800

    Update User.java

--- app/src/main/java/com/example/itp4203/room/model/User.java
+++ app/src/main/java/com/example/itp4203/room/model/User.java
@@ -20,6 +20,7 @@ public class User {
                 String email, String avatar, String facebookUrl, String instagramUrl,
                 String linkedinUrl, String twitterUrl, String bio,
                 boolean isSuperuser, boolean isInstructor) {
+        this.id = id;
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -33,5 +34,118 @@ public class User {
         this.bio = bio;
         this.isSuperuser = isSuperuser;
         this.isInstructor = isInstructor;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @NonNull
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(@NonNull String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getFacebookUrl() {
+        return facebookUrl;
+    }
+
+    public void setFacebookUrl(String facebookUrl) {
+        this.facebookUrl = facebookUrl;
+    }
+
+    public String getInstagramUrl() {
+        return instagramUrl;
+    }
+
+    public void setInstagramUrl(String instagramUrl) {
+        this.instagramUrl = instagramUrl;
+    }
+
+    public String getLinkedinUrl() {
+        return linkedinUrl;
+    }
+
+    public void setLinkedinUrl(String linkedinUrl) {
+        this.linkedinUrl = linkedinUrl;
+    }
+
+    public String getTwitterUrl() {
+        return twitterUrl;
+    }
+
+    public void setTwitterUrl(String twitterUrl) {
+        this.twitterUrl = twitterUrl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public boolean isSuperuser() {
+        return isSuperuser;
+    }
+
+    public void setSuperuser(boolean superuser) {
+        isSuperuser = superuser;
+    }
+
+    public boolean isInstructor() {
+        return isInstructor;
+    }
+
+    public void setInstructor(boolean instructor) {
+        isInstructor = instructor;
     }
 }
